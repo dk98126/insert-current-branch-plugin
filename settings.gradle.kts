cb1c1e8bac8985f6aa75840f7b1b0226980a7139
Daniil Krasnov <d.krasnov@hh.ru>
2025-06-05 13:23:23 +0300

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "insert-current-branch-plugin"
---
+++ src/main/kotlin/com/github/dk98126/insertcurrentbranchplugin/InsertBranchNameMessageAction.kt
@@ -0,0 +1,28 @@
+package com.github.dk98126.insertcurrentbranchplugin
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.VcsDataKeys
+import git4idea.repo.GitRepositoryManager
+
+class InsertBranchNameMessageAction : DumbAwareAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val commitMessageUi = e.getData(VcsDataKeys.COMMIT_WORKFLOW_UI)?.commitMessageUi ?: return
+        val currentBranch = getCurrentGitBranch(project) ?: return
+        var message = commitMessageUi.text
+        message = "$currentBranch $message"
+        commitMessageUi.text = message
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.EDT
+    }
+
+    private fun getCurrentGitBranch(project: Project): String? {
+        return GitRepositoryManager.getInstance(project).repositories.firstOrNull()?.currentBranchName
+    }
+}
